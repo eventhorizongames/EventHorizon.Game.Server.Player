0d3dc90f1b128879383ddc34bc3e8567e93dca73
Cody Merritt Anhorn <cody.anhorn@hotmail.com>
2018-12-15 10:37:01 -0600

    Added Name and Locale to base Player state Entity.

--- Model/PlayerEntity.cs
+++ Model/PlayerEntity.cs
@@ -8,6 +8,8 @@ namespace EventHorizon.Game.Server.Player.Model
         public static PlayerEntity NULL = default(PlayerEntity);
 
         public string Id { get; set; }
+        public string Name { get; set; }
+        public string Locale { get; set; }
         public PositionState Position { get; set; }
         public object Data { get; set; }
 
@@ -15,6 +17,8 @@ namespace EventHorizon.Game.Server.Player.Model
         {
             this.Id = id;
             this.Position = positionState;
+            this.Name = "";
+            this.Locale = "";
 
             this.Data = new { };
         }
